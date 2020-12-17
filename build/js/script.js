f031508928b1a62f3b31bfb328b6cc9935c544c1
A Zhila <aleksej.zhila@gmail.com>
2020-12-18 01:29:25 +0300

    Добавляет скрипт для попапа

--- build/js/script.js
+++ build/js/script.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var body = document.body;
+var popupButton = document.querySelector('.page-header__button');
+var popup = document.querySelector('.popup');
+var popupClose = popup.querySelector('.popup__close');
 var naviListButton = document.querySelector('.page-footer__list-button--navi');
 var naviList = document.querySelector('.page-footer__navi-list');
 var contactsListButton = document.querySelector('.page-footer__list-button--contacts');
@@ -8,6 +12,44 @@ var buttons = document.querySelectorAll('.page-footer__list-button');
 
 var MOBILE_WIDTH = 767;
 
+// скрипт для работы попапа
+popupButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  openPopup();
+});
+
+var openPopup = function () {
+  popup.classList.remove('popup--closed');
+  body.style.overflow = 'hidden';
+
+  popupClose.addEventListener('click', closePopup);
+  document.addEventListener('keydown', popupEscHandler);
+  document.addEventListener('click', overlayClickHandler);
+};
+
+var closePopup = function () {
+  popup.classList.add('popup--closed');
+  body.style.removeProperty('overflow-y');
+
+  popupClose.removeEventListener('click', closePopup);
+  document.removeEventListener('keydown', popupEscHandler);
+  document.removeEventListener('click', overlayClickHandler);
+};
+
+var popupEscHandler = function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    closePopup();
+  }
+};
+
+var overlayClickHandler = function (evt) {
+  if (evt.target.classList.contains('popup')) {
+    closePopup();
+  }
+};
+
+// скрипт для работы аккордеона
 if (window.innerWidth <= MOBILE_WIDTH) {
   naviList.classList.add('visually-hidden');
   contactsList.classList.add('visually-hidden');
@@ -62,10 +104,6 @@ var switchLists = function (evt) {
     }
   }
 };
-
-// buttons.forEach(function (button) {
-//   button.addEventListener('click', switchLists);
-// });
 
 for (var i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', switchLists);
